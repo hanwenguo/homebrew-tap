0412a4712ee7902d78ba2f7d223fdd70209d16bc
Hanwen Guo <github@hanwen.io>
2026-08-04 06:58:44 +0000

    weibian: add 2.0.0-rc.5 bottle.

--- Formula/weibian.rb
+++ Formula/weibian.rb
@@ -4,6 +4,14 @@ class Weibian < Formula
   version "2.0.0-rc.5"
   license "GPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/hanwenguo/homebrew-tap/releases/download/weibian-2.0.0-rc.5"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "a9554082c0e729d85d1cc9c34852bcb72014137eac7b2d0a8f24c63a43c694dd"
+    sha256 cellar: :any_skip_relocation, sequoia:       "ad729b339d5d174e7f4742329d6e88dc99ed3a2f8898c0f4ab384ae7d1eaf311"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "593de0163afc1708babeb3dd2f2a4e3ac537c9e4e12fb9a35b099344f301d4cb"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "a7d96918643b389e6c3b67cc2f1ec13bd00f55dffd76b21ef8d798829d1a6b9e"
+  end
+
   on_macos do
     on_arm do
       url "https://github.com/hanwenguo/weibian/releases/download/v2.0.0-rc.5/wb-aarch64-apple-darwin.tar.gz"

3827fd8fcd071c793eef41a4fb59bcb48d0fdafb
Hanwen Guo <guo@hanwen.io>
2026-08-04 01:50:43 -0400

    ci: package native release binaries
    Closes #2.
    
    Signed-off-by: Hanwen Guo <github@hanwen.io>

--- Formula/lantai.rb
+++ Formula/lantai.rb
@@ -1,15 +1,33 @@
 class Lantai < Formula
   desc "BibLaTeX-backed headless reference manager"
   homepage "https://github.com/hanwenguo/lantai"
-  url "https://github.com/hanwenguo/lantai/archive/refs/tags/v0.5.0.tar.gz"
-  sha256 "13510c59f9160261c8884416ea3f5bcb9e69b4cdcf4f4095ccd6cc156e98384d"
   license "AGPL-3.0-only"
   revision 1
 
-  depends_on "rust" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/hanwenguo/lantai/releases/download/v0.5.0/lantai-0.5.0-aarch64-apple-darwin.tar.gz"
+      sha256 "a37caf85aaf492342081607df520c49b04eacf8fe45b7fb4d3612a3637b1061a"
+    end
+    on_intel do
+      url "https://github.com/hanwenguo/lantai/releases/download/v0.5.0/lantai-0.5.0-x86_64-apple-darwin.tar.gz"
+      sha256 "5169aed9af119f7a89a183635f42c19331eb499e5c3444ed6d811bd4730e0592"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/hanwenguo/lantai/releases/download/v0.5.0/lantai-0.5.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "c6a974e865e980027ea2ff5f19f09d12c22815da20c097027a6587a57a24ec1b"
+    end
+    on_intel do
+      url "https://github.com/hanwenguo/lantai/releases/download/v0.5.0/lantai-0.5.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "0b9b012799962ae9920670db6db2ff876c88d269f3a978040d46eb863e9af789"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "lantai"
   end
 
   test do
--- Formula/weibian.rb
+++ Formula/weibian.rb
@@ -1,15 +1,33 @@
 class Weibian < Formula
   desc "Typst document compiler and site builder"
   homepage "https://github.com/hanwenguo/weibian"
-  url "https://github.com/hanwenguo/weibian/archive/refs/tags/v2.0.0-rc.3.tar.gz"
-  sha256 "fd77c90097e993f775e8cc25e6a9b59bfa8cb25d6922fd1e08b24a7a4698ceee"
+  version "2.0.0-rc.4"
   license "GPL-3.0-only"
-  revision 1
 
-  depends_on "rust" => :build
+  on_macos do
+    on_arm do
+      url "https://github.com/hanwenguo/weibian/releases/download/v2.0.0-rc.4/wb-aarch64-apple-darwin.tar.gz"
+      sha256 "fbc998a847d8cde4bde82f0ba3aec35fef643df58dae249685e7f97d3f19c643"
+    end
+    on_intel do
+      url "https://github.com/hanwenguo/weibian/releases/download/v2.0.0-rc.4/wb-x86_64-apple-darwin.tar.gz"
+      sha256 "1eea4d625a4efae0e52f94ae706bcdbd1cdc3b3c37b72a202e7aaa3da441c763"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/hanwenguo/weibian/releases/download/v2.0.0-rc.4/wb-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "865854286bd55d44ee5f6efe9388adb015b4b84ce0182ff5a437cbcb6014604d"
+    end
+    on_intel do
+      url "https://github.com/hanwenguo/weibian/releases/download/v2.0.0-rc.4/wb-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "c13b5f19093dc9f101d47ad41bd67c4778c4980f96985bb10a70bf0f0eaf2dc4"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "wb"
   end
 
   test do

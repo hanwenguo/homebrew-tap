1ded3036ff3a22504cdede71d7882794f1ca1908
Hanwen Guo <github@hanwen.io>
2026-08-04 06:01:00 +0000

    lantai: add 0.5.0_1 bottle.

--- Formula/lantai.rb
+++ Formula/lantai.rb
@@ -4,6 +4,14 @@ class Lantai < Formula
   license "AGPL-3.0-only"
   revision 1
 
+  bottle do
+    root_url "https://github.com/hanwenguo/homebrew-tap/releases/download/lantai-0.5.0_1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "b97a790f3c3e5feb8be54e336490ddcd258eb5a15daddff0d02a0cb516c27808"
+    sha256 cellar: :any_skip_relocation, sequoia:       "c11606f2418cf1daa369e10f62a8b3e837fcab5bae68d31665a3b731b4480b22"
+    sha256 cellar: :any_skip_relocation, arm64_linux:   "077f06bf0efb957878ed5e708fca54a067cc0cf1eef8ad30afe710b355180421"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "59e6510d7b38cecbca08b9c29201786ff905e47181bc76a0a1b75dd4805b719c"
+  end
+
   on_macos do
     on_arm do
       url "https://github.com/hanwenguo/lantai/releases/download/v0.5.0/lantai-0.5.0-aarch64-apple-darwin.tar.gz"

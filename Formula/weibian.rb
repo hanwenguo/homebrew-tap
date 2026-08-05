d376e92553e7938c1b5489c51602eaf355b890ce
Hanwen Guo <github@hanwen.io>
2026-08-05 01:41:25 +0000

    weibian: add 2.0.0-rc.5_2 bottle.

--- Formula/weibian.rb
+++ Formula/weibian.rb
@@ -11,6 +11,14 @@ class Weibian < Formula
     regex(/^v?(\d+(?:\.\d+)+(?:-rc\.\d+)?)$/i)
   end
 
+  bottle do
+    root_url "https://github.com/hanwenguo/homebrew-tap/releases/download/weibian-2.0.0-rc.5_2"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:   "2a2997b968d5e85d37afff125a181fd9f86e231289caba9e5e981ce8022af54d"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "183f2911aa0eee1a7911579a511ed575b205d3a346a4dbf1345c033455183682"
+    sha256 cellar: :any,                 arm64_linux:   "c2a50d1e476c60869df98950738caa44c9f3f726c9eb4815b7331c8611cc6782"
+    sha256 cellar: :any,                 x86_64_linux:  "ec33a27e2544578bbb2ca1d2db06fadc98ffe0f902f6477fc3626317fce3e809"
+  end
+
   depends_on "rust" => :build
 
   on_macos do

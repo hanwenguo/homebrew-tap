214288f254974835fe4ff29bdf1c7c994614be69
Hanwen Guo <github@hanwen.io>
2026-08-10 20:51:20 +0000

    lantai: add 0.7.0 bottle.

--- Formula/lantai.rb
+++ Formula/lantai.rb
@@ -5,6 +5,13 @@ class Lantai < Formula
   sha256 "bcac94c68f70a3919dab3ddf022fb63eed62e60bd37561c84474522a49966181"
   license "AGPL-3.0-only"
 
+  bottle do
+    root_url "https://github.com/hanwenguo/homebrew-tap/releases/download/lantai-0.7.0"
+    sha256 cellar: :any_skip_relocation, arm64_tahoe:  "af47f5b0254cc17a8f4c9804f4cb8ef1139e60ac754b3cd578c711626ba25dcb"
+    sha256 cellar: :any,                 arm64_linux:  "24857e124ba76f48b160290e9530815a683123250bc46b03b7974ddeb78b3954"
+    sha256 cellar: :any,                 x86_64_linux: "364f3177de5df9068d65722ce23a608004c95ac549e76a570b9e8b4a5fc972a5"
+  end
+
   depends_on "rust" => :build
 
   on_macos do
